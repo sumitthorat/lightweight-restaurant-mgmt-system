99cfe0bc8945c8cbbba2e7cfba4d2d0ac710f1d3
Sumit Thorat <sumitthorat@Sumits-MacBook-Pro.local>
2020-10-27 12:41:13 +0530

    Initial Commit

---
+++ Frontend/Android/app/src/main/java/com/example/lmrs/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.lmrs.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lmrs.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
