72589b8af314366d1616d130b486bdeff2e136dd
ttod04 <raj.ankit141@gmail.com>
2020-11-18 09:14:26 +0530

    Added comments in Webapp

--- Backend/Webapp/PROJECT/order/views.py
+++ Backend/Webapp/PROJECT/order/views.py
@@ -5,7 +5,7 @@ import requests
 def sortFunction(value):
     return value["category"]
 
-
+#Get the menu and tables info which will be used in the html page
 def menu_show(request):
     response = requests.get('http://localhost:5000/GetFullMenu')
     response2 = requests.get('http://localhost:5000/GetAllTablesInfo')
